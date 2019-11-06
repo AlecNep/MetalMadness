741f0e06154be9620d85f940d85b26303d60db25
AlecNep <anepute@live.com>
2019-11-05 21:18:54 -0500

    Much more solid command pattern groundwork

--- Assets/Scripts/CommandPattern.cs
+++ Assets/Scripts/CommandPattern.cs
@@ -7,12 +7,15 @@ namespace CommandPattern
 
     public abstract class Command
     {
+        public GameObject mPlayer;
         //Parent function
         public abstract void Execute();
     }
 
-    //Child classes
+    //~~~Child classes~~~
 
+    
+    //Active gameplay commands
     public class Jump : Command
     {
         public override void Execute()
@@ -52,6 +55,85 @@ namespace CommandPattern
 
         }
     }
+
+    public class OpenMap : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+    //End active gameplay commands
+
+
+    //Menu commands
+    public class Select : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+
+    public class Cancel : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+
+    public class NextTab : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+
+    public class PreviousTab : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+    //End menu commands
+
+    //Map commands
+    public class PlaceMarker : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+
+    public class Close : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+
+    public class ZoomIn : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+
+    public class ZoomOut : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+    //End map commands
+
 
     public class DoNothing : Command
     {
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,7 +8,11 @@ namespace CommandPattern
     {
 
         //Player transform
-        public Rigidbody mPlayerRB;
+        //public Rigidbody mPlayerRB;
+
+        //Game mode
+        public enum GameModes { Gameplay = 0, Menu = 1, Map = 2};
+        public GameModes mGameMode = GameModes.Gameplay;
         //Controls on a basic Xbox 360 controller
         private Command mAButton, mBButton, mXButton, mYButton, mRBumper, mLBumper, mUp, mDown, mLeft, mRight; //mRTrigger, mLTrigger; //not sure if/how well these will work here since they're triggers
 
@@ -17,13 +21,79 @@ namespace CommandPattern
         void Start()
         {
             //Default bindings
-            //mAButton = 
+            mAButton = new Jump();
+            mBButton = new Dash();
+            mXButton = new Attack();
+            mYButton = new SwapWeapon();
+            mRBumper = new WeaponWheel();
+            mLBumper = new OpenMap();
+
+            mUp = new DoNothing();
+            mDown = new DoNothing();
+            mLeft = new DoNothing();
+            mRight = new DoNothing();
         }
 
         // Update is called once per frame
         void Update()
         {
+            HandleInput();
+        }
 
+        public void HandleInput()
+        {
+            //Maybe put this somewhere else
+            //The buttons stay the same, it's their context that changes
+            /*switch ((int)mGameMode)
+            {
+                case 0: //Active gameplay
+                    
+                    break;
+                case 1: //Menu
+                    break;
+                case 2: //Map
+                    break;
+            }*/
+            if (Input.GetButtonDown("AButton"))
+            {
+                mAButton.Execute();
+            }
+            if (Input.GetButtonDown("BButton"))
+            {
+                mBButton.Execute();
+            }
+            if (Input.GetButtonDown("XButton"))
+            {
+                mXButton.Execute();
+            }
+            if (Input.GetButtonDown("YButton"))
+            {
+                mYButton.Execute();
+            }
+            if (Input.GetButtonDown("RBumper"))
+            {
+                mRBumper.Execute();
+            }
+            if (Input.GetButtonDown("LBumper"))
+            {
+                mLBumper.Execute();
+            }
+            if (Input.GetAxis("DPadX") > 0)
+            {
+                mRight.Execute();
+            }
+            if (Input.GetAxis("DPadX") < 0)
+            {
+                mLeft.Execute();
+            }
+            if (Input.GetAxis("DPadY") > 0)
+            {
+                mUp.Execute();
+            }
+            if (Input.GetAxis("DPadY") < 0)
+            {
+                mDown.Execute();
+            }
         }
     }
 }
