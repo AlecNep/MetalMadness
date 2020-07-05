0915b1763ff26a3f71d96ba99de7348dab56e2bc
AlecNep <anepute@live.com>
2020-07-05 15:49:36 -0400

    Got the stickiness and flashing lights on the sticky bomb mostly working now. Still don't know why the bombs maintain their orientation before hitting a surface

--- Assets/Scripts/StickyBomb.cs
+++ Assets/Scripts/StickyBomb.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class StickyBomb : Bullet {
 
-    bool mDetonation = false;
+    //bool mDetonation = false;
+    private IEnumerator mDetonation;
+    private GameObject mLight;
 
 	// Use this for initialization
 	void Start () {
-		
+        mRb.centerOfMass = new Vector3(0, 0.15f, 0); //doesn't seem to work
+        mDetonation = DetonationSequence();
+        mLight = transform.GetChild(transform.childCount - 1).gameObject;
 	}
 	
 	// Update is called once per frame
@@ -21,8 +25,29 @@ public class StickyBomb : Bullet {
         mRb.AddForce(pDir * mSpeed, ForceMode.Impulse);
     }
 
-    /*public new void OnCollisionEnter(Collision col)
+    public new void OnCollisionEnter(Collision col)
     {
+        if (col.rigidbody)
+        {
+            gameObject.AddComponent<HingeJoint>();
+            GetComponent<HingeJoint>().connectedBody = col.rigidbody;
+            StartCoroutine(mDetonation);
+        }
+    }
 
-    }*/
+    private IEnumerator DetonationSequence()
+    {
+        yield return new WaitForSeconds(0.5f);
+        for(int i = 0; i < 2; i++)
+        {
+            mLight.SetActive(false);
+            yield return new WaitForSeconds(0.05f);
+            mLight.SetActive(true);
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return new WaitForSeconds(0.1f);
+
+        //TODO: Explosion
+        Destroy(gameObject);
+    }
 }
