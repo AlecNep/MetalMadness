cc18eec35cabe7fd31f31cb77f37cec9e02797c2
AlecNep <anepute@live.com>
2020-11-15 20:15:56 -0500

    added a compass, going to try to stop momentum when shifting gravity

---
+++ Assets/Scripts/UI/Compass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour {
+
+    private PlayerControls mPlayer;
+    private RectTransform mArrow;
+
+	// Use this for initialization
+	void Start () {
+        mPlayer = GameObject.Find("Player").GetComponent<PlayerControls>();
+        mArrow = transform.GetChild(0).GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mArrow.rotation = Quaternion.Euler(Vector3.forward * -90 * (int)mPlayer.mCurGravity);
+        //negative because the rotation for the rect transform is backwards
+        //why in the ever-loving Christ is it backwards?????
+	}
+}
