5cab3d6685c73995c9a4cc291e273e9fdf030665
AlecNep <anepute@live.com>
2021-12-12 15:20:14 -0500

    Added a sample for a default camera

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float defaultDistance;
+    [SerializeField]
+    private float heightAbovePlayer;
+    [SerializeField]
+    private float angle;
+    [SerializeField]
+    private float rotationSpeed;
+
+    private Transform customCam;
+    private PlayerControls playerRef;
+    private 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        customCam = transform.Find("Camera");
+        if (customCam == null)
+            Debug.LogError("Somehow the custom camera doesn't have a camera child object");
+        playerRef = GameManager.Instance.player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerRef.transform.position + Vector3.forward * defaultDistance;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * playerRef.mGravShifter.GetShiftAngle()), rotationSpeed);
+        customCam.localPosition = Vector3.up * heightAbovePlayer;
+        customCam.localRotation = Quaternion.Euler(Vector3.right * angle);
+    }
+}
