7e61b8594edecc9b80690bb0435c345ca5c5d7fd
AlecNep <anepute@live.com>
2022-01-12 18:27:48 -0500

    Updated the 2nd sample scene, fixed a bug with the base hazard script, and more small fixes

--- Assets/Scripts/Environment/DamageHazard.cs
+++ Assets/Scripts/Environment/DamageHazard.cs
@@ -16,8 +16,25 @@ public class DamageHazard : MonoBehaviour
     {
         if (other.tag == "Player" || other.tag == "Enemy")
         {
-            Damageable dmg = other.GetComponent<Damageable>();
-            dmg.ChangeHealth(-damage);
+            if (other.TryGetComponent(out Damageable d))
+            {
+                //Damageable dmg = other.GetComponent<Damageable>();
+                d.ChangeHealth(-damage);
+            }
+            else 
+            {
+                Damageable d2 = other.gameObject.GetComponentInParent<Damageable>();
+
+                if (d2 != null)
+                {
+                    d2.ChangeHealth(-damage);
+                }
+                else
+                {
+                    Debug.LogError(other.name + "unable to take damage");
+                }
+            }
+            
         }
     }
 
@@ -28,10 +45,29 @@ public class DamageHazard : MonoBehaviour
     /// <param name="collision"></param>
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.collider.tag == "Player" || collision.collider.tag == "Enemy")
+        /*if (collision.collider.tag == "Player" || collision.collider.tag == "Enemy")
         {
             Damageable dmg = collision.collider.GetComponent<Damageable>();
             dmg.ChangeHealth(-damage);
+        }*/
+        GameObject other = collision.gameObject;
+        if (other.TryGetComponent(out Damageable d))
+        {
+            //Damageable dmg = other.GetComponent<Damageable>();
+            d.ChangeHealth(-damage);
+        }
+        else
+        {
+            Damageable d2 = other.GetComponentInParent<Damageable>();
+
+            if (d2 != null)
+            {
+                d2.ChangeHealth(-damage);
+            }
+            else
+            {
+                Debug.LogError(other.name + "unable to take damage");
+            }
         }
     }
 }
